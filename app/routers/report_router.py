2640079e2b3e1688cf32bfa0377fabb3b9728920
masu11 <marcel.suchomel@gmail.com>
2026-03-03 08:44:57 +0100

    Delete app/routers directory

--- app/routers/report_router.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-from app.services.weekly_report_service import get_weekly_report
-
-router = APIRouter()
-
-@router.get("/weekly-report")
-def weekly_report():
-    return get_weekly_report()
