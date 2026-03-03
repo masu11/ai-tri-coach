1f0741d987b14e8864122453cc42d35577febc8a
masu11 <marcel.suchomel@gmail.com>
2026-03-03 09:52:50 +0100

    a

---
+++ app/models.py
@@ -0,0 +1,26 @@
+from sqlalchemy import Column, Integer, String, Float, Date, DateTime
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+class Activity(Base):
+    __tablename__ = "activities"
+
+    id = Column(Integer, primary_key=True)
+    tss = Column(Float)
+    start_date = Column(DateTime)
+
+class ActivityStream(Base):
+    __tablename__ = "activity_streams"
+
+    id = Column(Integer, primary_key=True)
+    activity_id = Column(Integer)
+
+class GarminDailyMetrics(Base):
+    __tablename__ = "garmin_daily_metrics"
+
+    id = Column(Integer, primary_key=True)
+    date = Column(Date)
+    avg_hrv = Column(Float)
+    resting_hr = Column(Float)
+    sleep_score = Column(Float)
