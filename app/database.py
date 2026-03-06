84fba7b8504a0d3c52b874d60225cccc56b91093
masu11 <marcel.suchomel@gmail.com>
2026-03-06 09:07:49 +0100

    a

--- app/database.py
+++ app/database.py
@@ -8,3 +8,46 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
+import psycopg2
+import os
+
+
+class Database:
+
+    def get_conn(self):
+        return psycopg2.connect(os.getenv("DATABASE_URL"))
+
+
+    def fetch_all(self, query):
+
+        with self.get_conn() as conn:
+            with conn.cursor() as cur:
+
+                cur.execute(query)
+
+                cols = [c[0] for c in cur.description]
+
+                rows = cur.fetchall()
+
+                return [dict(zip(cols, r)) for r in rows]
+
+
+    def fetch_one(self, query):
+
+        with self.get_conn() as conn:
+            with conn.cursor() as cur:
+
+                cur.execute(query)
+
+                cols = [c[0] for c in cur.description]
+
+                row = cur.fetchone()
+
+                if not row:
+                    return None
+
+                return dict(zip(cols, row))
+
+
+db = Database()
