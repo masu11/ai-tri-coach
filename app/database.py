00eb321a684c4ef7d96c67ae4ae6aebe8dfee7d0
masu11 <marcel.suchomel@gmail.com>
2026-03-06 17:49:11 +0100

    A

--- app/database.py
+++ app/database.py
@@ -5,7 +5,11 @@ from sqlalchemy.orm import declarative_base
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True,
+    pool_recycle=300
+)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
